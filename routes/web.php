469416f174f677903aeb574c04762c9e8ccd4eda
3A732001 <3a732001@gm.student.ncut.edu.tw>
2020-12-21 18:42:31 +0800

    dd.d2

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,10 @@ Route::get('dd/d1', function () {
     return view('dd.d1');
 });
 
+Route::get('dd/d2', function () {
+    return view('dd.d2');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
